627793b06a14a6d1801c76b37605cbc55bd19fa4
chrisjc9931 <>
2021-06-07 02:56:32 +0000

    added validation that i think sucks

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ function removeI(arr) {
 let arrayToChange = ['One', 'i', 'c', 'X', 'i', 'i', 54];
 
 console.log(removeI(arrayToChange));
+
+//The following concept check assumes that only positive integers are passed to the function.
+
+function factorial(integer){
+  while (checkIfNumber(integer) === false){
+  if (integer === 1){
+    return integer;
+  } else {
+    return integer*(factorial(integer-1));
+  }
+}
+}
+
+
+function checkIfNumber(input){
+  if(typeof(input) == "string" || input !== Math.floor(input) || input < 0){
+    console.log(`Please enter a whole integer`);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+console.log(factorial(-1));
+
+//Skill boost! Add validation to return an error message if the function is passed a string, negative number or a decimal value.
